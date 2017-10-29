9e4c330aef4425afb6796fa9e3975d410670b4a8
ptslog <hsfk@yandex.ru>
2017-10-29 11:46:22 +1000

    add value

---
+++ lib/libscl/value.c
@@ -0,0 +1,589 @@
+#include "value.h"
+#include "misc.h"
+#include <math.h>
+
+extern void float_init_sp(float_value* self, float v)
+{
+        self->_precision = FP_SINGLE;
+        self->_float     = v;
+}
+
+extern void float_init_dp(float_value* self, double v)
+{
+        self->_precision = FP_DOUBLE;
+        self->_double    = v;
+}
+
+extern float_precision float_get_precision(const float_value* self)
+{
+        return self->_precision;
+}
+
+extern op_result float_add(float_value* self, const float_value* rhs)
+{
+        if (float_is_sp(self))
+                self->_float += float_get_sp(rhs);
+        else
+                self->_double += float_get_dp(rhs);
+
+        return OR_OK;
+}
+
+extern op_result float_sub(float_value* self, const float_value* rhs)
+{
+        if (float_is_sp(self))
+                self->_float -= float_get_sp(rhs);
+        else
+                self->_double -= float_get_dp(rhs);
+
+        return OR_OK;
+}
+
+extern op_result float_mul(float_value* self, const float_value* rhs)
+{
+        if (float_is_sp(self))
+                self->_float *= float_get_sp(rhs);
+        else
+                self->_double *= float_get_dp(rhs);
+
+        return OR_OK;
+}
+
+extern op_result float_div(float_value* self, const float_value* rhs)
+{
+        if (float_is_zero(rhs))
+                return OR_DIV_BY_ZERO;
+
+        if (float_is_sp(self))
+                self->_float /= float_get_sp(rhs);
+        else
+                self->_double /= float_get_dp(rhs);
+
+        return OR_OK;
+}
+
+extern cmp_result float_cmp(const float_value* lhs, const float_value* rhs)
+{
+        if (float_is_sp(lhs))
+        {
+                float lv = float_get_sp(lhs);
+                float rv = float_get_sp(rhs);
+                if (lv == rv)
+                        return CR_EQ;
+                return lv > rv ? CR_GR : CR_LE;
+        }
+        else
+        {
+                double lv = float_get_dp(lhs);
+                double rv = float_get_dp(rhs);
+                if (lv == rv)
+                        return CR_EQ;
+                return lv > rv ? CR_GR : CR_LE;
+        }
+}
+
+extern bool float_is_zero(const float_value* val)
+{
+        if (float_is_sp(val))
+                return float_get_sp(val) == 0.0f;
+        else
+                return float_get_dp(val) == 0.0;
+}
+
+extern bool float_is_inf(const float_value* val)
+{
+        if (float_is_sp(val))
+                return isinf(float_get_sp(val));
+        else
+                return isinf(float_get_dp(val));
+}
+
+extern bool float_is_nan(const float_value* val)
+{
+        if (float_is_sp(val))
+                return isnan(float_get_sp(val));
+        else
+                return isnan(float_get_dp(val));
+}
+
+extern bool float_is_sp(const float_value* val)
+{
+        return float_get_precision(val) == FP_SINGLE;
+}
+
+extern bool float_is_dp(const float_value* val)
+{
+        return float_get_precision(val) == FP_DOUBLE;
+}
+
+extern void float_to_dp(float_value* val)
+{
+        if (float_is_sp(val))
+                float_init_dp(val, float_get_dp(val));
+}
+
+extern void float_to_sp(float_value* val)
+{
+        if (float_is_dp(val))
+                float_init_sp(val, float_get_sp(val));
+}
+
+extern int_value float_to_int(const float_value* val, uint bits)
+{
+        suint64 v = float_get64(val);
+        int_value iv;
+        int_init(&iv, bits, v);
+        return iv;
+}
+
+extern float float_get_sp(const float_value* val)
+{
+        if (float_is_dp(val))
+                return (float)val->_double;
+        else
+                return val->_float;
+
+}
+
+extern double float_get_dp(const float_value* val)
+{
+        if (float_is_sp(val))
+                return (double)val->_float;
+        else
+                return val->_double;
+}
+
+extern suint8 float_get8(const float_value* val)
+{
+        if (float_is_sp(val))
+                return (suint8)val->_float;
+        else
+                return (suint8)val->_double;
+}
+
+extern suint16 float_get16(const float_value* val)
+{
+        if (float_is_sp(val))
+                return (suint16)val->_float;
+        else
+                return (suint16)val->_double;
+}
+
+extern suint32 float_get32(const float_value* val)
+{
+        if (float_is_sp(val))
+                return (suint32)val->_float;
+        else
+                return (suint32)val->_double;
+}
+
+extern suint64 float_get64(const float_value* val)
+{
+        if (float_is_sp(val))
+                return (suint64)val->_float;
+        else
+                return (suint64)val->_double;
+}
+
+extern void int_init(int_value* self, uint bits, suint64 val)
+{
+        self->_val  = val;
+        int_resize(self, bits);
+}
+
+extern uint int_get_bits(const int_value* self)
+{
+        return self->_bits;
+}
+
+extern op_result int_add(int_value* self, const int_value* rhs)
+{
+        suint64 x  = int_get64(self);
+        suint64 y  = int_get64(rhs);
+        self->_val = mod2(x + y, int_get_bits(self));
+        return self->_val < x ? OR_OVERFLOW : OR_OK;
+}
+
+extern op_result int_sub(int_value* self, const int_value* rhs)
+{
+        suint64 x  = int_get64(self);
+        suint64 y  = int_get64(rhs);
+        self->_val = mod2(x - y, int_get_bits(self));
+        return self->_val > x ? OR_UNDERFLOW : OR_OK;
+}
+
+extern op_result int_mul(int_value* self, const int_value* rhs)
+{
+        suint64 x  = int_get64(self);
+        suint64 y  = int_get64(rhs);
+        self->_val = mod2(x * y, int_get_bits(self));
+
+        return x != 0 && self->_val / x != y
+                ? OR_OVERFLOW : OR_OK;
+}
+
+extern op_result int_div(int_value* self, const int_value* rhs)
+{
+        suint64 x = int_get64(self);
+        suint64 y = int_get64(rhs);
+        if (y == 0)
+                return OR_DIV_BY_ZERO;
+
+        self->_val = mod2(x / y, int_get_bits(self));
+        return OR_OK;
+}
+
+extern op_result int_mod(int_value* self, const int_value* rhs)
+{
+        suint64 x = int_get64(self);
+        suint64 y = int_get64(rhs);
+        if (y == 0)
+                return OR_DIV_BY_ZERO;
+
+        self->_val = mod2(x % y, int_get_bits(self));
+        return OR_OK;
+}
+
+extern op_result int_shl(int_value* self, const int_value* rhs)
+{
+        suint64 x  = int_get64(self);
+        suint64 y  = int_get64(rhs);
+        self->_val = mod2(x << y, int_get_bits(self));
+        return self->_val >> y == x ? OR_OK : OR_OVERFLOW;
+}
+
+extern op_result int_shr(int_value* self, const int_value* rhs)
+{
+        suint64 x  = int_get64(self);
+        suint64 y  = int_get64(rhs);
+        self->_val = x >> y;
+        return self->_val << y == x ? OR_OK : OR_UNDERFLOW;
+}
+
+extern op_result int_and(int_value* self, const int_value* rhs)
+{
+        self->_val &= int_get64(rhs);
+        return OR_OK;
+}
+
+extern op_result int_xor(int_value* self, const int_value* rhs)
+{
+        self->_val ^= mod2(int_get64(rhs), int_get_bits(self));
+        return OR_OK;
+}
+
+extern op_result int_or(int_value* self, const int_value* rhs)
+{
+        self->_val |= mod2(int_get64(rhs), int_get_bits(self));
+        return OR_OK;
+}
+
+extern cmp_result int_cmp(const int_value* lhs, const int_value* rhs)
+{
+        if (int_get64(lhs) == int_get64(rhs))
+                return CR_EQ;
+        return int_get64(lhs) > int_get64(rhs) ? CR_GR : CR_LE;
+}
+
+extern bool int_is_zero(const int_value* val)
+{
+        return int_get64(val) == 0;
+}
+
+extern suint8 int_get8(const int_value* val)
+{
+        return (suint8)int_get64(val);
+}
+
+extern suint16 int_get16(const int_value* val)
+{
+        return (suint16)int_get64(val);
+}
+
+extern suint32 int_get32(const int_value* val)
+{
+        return (suint32)int_get64(val);
+}
+
+extern suint64 int_get64(const int_value* val)
+{
+        return val->_val;
+}
+
+extern suint64 int_get_max(const int_value* val)
+{
+        return ((suint64)-1) >> (64 - val->_bits);
+}
+
+extern float int_get_sp(const int_value* val)
+{
+        return (float)int_get64(val);
+}
+
+extern double int_get_dp(const int_value* val)
+{
+        return (double)int_get64(val);
+}
+
+extern void int_resize(int_value* val, uint bits)
+{
+        S_ASSERT(bits && bits <= 64);
+        val->_bits = bits;
+        val->_val  = mod2(val->_val, bits);
+}
+
+extern float_value int_to_sp(const int_value* val)
+{
+        float_value fv;
+        float_init_sp(&fv, int_get_sp(val));
+        return fv;
+}
+
+extern float_value int_to_dp(const int_value* val)
+{
+        float_value fv;
+        float_init_dp(&fv, int_get_dp(val));
+        return fv;
+}
+
+extern void avalue_init_int(avalue* self, uint bits, suint64 val)
+{
+        self->_integer = true;
+        int_init(&self->_int, bits, val);
+}
+
+extern void avalue_init_sp(avalue* self, float v)
+{
+        self->_integer = false;
+        float_init_sp(&self->_float, v);
+}
+
+extern void avalue_init_dp(avalue* self, double v)
+{
+        self->_integer = false;
+        float_init_dp(&self->_float, v);
+}
+
+extern op_result avalue_add(avalue* self, const avalue* rhs)
+{
+        avalue r = *rhs;
+        if (avalue_is_int(self))
+        {
+                avalue_to_int(&r, int_get_bits(&self->_int));
+                return int_add(&self->_int, &r._int);
+        }
+
+        avalue_to_dp(&r);
+        return float_add(&self->_float, &r._float);
+}
+
+extern op_result avalue_sub(avalue* self, const avalue* rhs)
+{
+        avalue r = *rhs;
+        if (avalue_is_int(self))
+        {
+                avalue_to_int(&r, int_get_bits(&self->_int));
+                return int_sub(&self->_int, &r._int);
+        }
+
+        avalue_to_dp(&r);
+        return float_sub(&self->_float, &r._float);
+}
+
+extern op_result avalue_mul(avalue* self, const avalue* rhs)
+{
+        avalue r = *rhs;
+        if (avalue_is_int(self))
+        {
+                avalue_to_int(&r, int_get_bits(&self->_int));
+                return int_mul(&self->_int, &r._int);
+        }
+
+        avalue_to_dp(&r);
+        return float_mul(&self->_float, &r._float);
+}
+
+extern op_result avalue_div(avalue* self, const avalue* rhs)
+{
+        avalue r = *rhs;
+        if (avalue_is_int(self))
+        {
+                avalue_to_int(&r, int_get_bits(&self->_int));
+                return int_div(&self->_int, &r._int);
+        }
+
+        avalue_to_dp(&r);
+        return float_div(&self->_float, &r._float);
+}
+
+extern op_result avalue_mod(avalue* self, const avalue* rhs)
+{
+        if (avalue_is_float(rhs))
+                return OR_INVALID;
+
+        avalue r = *rhs;
+        avalue_to_int(&r, int_get_bits(&self->_int));
+        return int_mod(&self->_int, &r._int);
+}
+
+extern op_result avalue_shl(avalue* self, const avalue* rhs)
+{
+        if (avalue_is_float(rhs))
+                return OR_INVALID;
+
+        avalue r = *rhs;
+        avalue_to_int(&r, int_get_bits(&self->_int));
+        return int_shl(&self->_int, &r._int);
+}
+
+extern op_result avalue_shr(avalue* self, const avalue* rhs)
+{
+        if (avalue_is_float(rhs))
+                return OR_INVALID;
+
+        avalue r = *rhs;
+        avalue_to_int(&r, int_get_bits(&self->_int));
+        return int_shr(&self->_int, &r._int);
+}
+
+extern op_result avalue_and(avalue* self, const avalue* rhs)
+{
+        if (avalue_is_float(rhs))
+                return OR_INVALID;
+
+        avalue r = *rhs;
+        avalue_to_int(&r, int_get_bits(&self->_int));
+        return int_and(&self->_int, &r._int);
+}
+
+extern op_result avalue_xor(avalue* self, const avalue* rhs)
+{
+        if (avalue_is_float(rhs))
+                return OR_INVALID;
+
+        avalue r = *rhs;
+        avalue_to_int(&r, int_get_bits(&self->_int));
+        return int_xor(&self->_int, &r._int);
+}
+
+extern op_result avalue_or(avalue* self, const avalue* rhs)
+{
+        if (avalue_is_float(rhs))
+                return OR_INVALID;
+
+        avalue r = *rhs;
+        avalue_to_int(&r, int_get_bits(&self->_int));
+        return int_or(&self->_int, &r._int);
+}
+
+extern cmp_result avalue_cmp(const avalue* lhs, const avalue* rhs)
+{
+        avalue r = *rhs;
+        if (avalue_is_float(lhs))
+        {
+                avalue_to_dp(&r);
+                return float_cmp(&lhs->_float, &r._float);
+        }
+
+        avalue_to_int(&r, int_get_bits(&lhs->_int));
+        return int_cmp(&lhs->_int, &r._int);
+}
+
+extern bool avalue_is_zero(const avalue* val)
+{
+        if (avalue_is_float(val))
+                return float_is_zero(&val->_float);
+        else
+                return int_is_zero(&val->_int);
+}
+
+extern bool avalue_is_float(const avalue* val)
+{
+        return !val->_integer;
+}
+
+extern bool avalue_is_int(const avalue* val)
+{
+        return !avalue_is_float(val);
+}
+
+extern suint8 avalue_get8(const avalue* val)
+{
+        if (avalue_is_float(val))
+                return float_get8(&val->_float);
+        else
+                return int_get8(&val->_int);
+}
+
+extern suint16 avalue_get16(const avalue* val)
+{
+        if (avalue_is_float(val))
+                return float_get16(&val->_float);
+        else
+                return int_get16(&val->_int);
+}
+
+extern suint32 avalue_get32(const avalue* val)
+{
+        if (avalue_is_float(val))
+                return float_get32(&val->_float);
+        else
+                return int_get32(&val->_int);
+}
+
+extern suint64 avalue_get64(const avalue* val)
+{
+        if (avalue_is_float(val))
+                return float_get64(&val->_float);
+        else
+                return int_get64(&val->_int);
+}
+
+extern float avalue_get_sp(const avalue* val)
+{
+        if (avalue_is_float(val))
+                return float_get_sp(&val->_float);
+        else
+                return int_get_sp(&val->_int);
+}
+
+extern double avalue_get_dp(const avalue* val)
+{
+        if (avalue_is_float(val))
+                return float_get_dp(&val->_float);
+        else
+                return int_get_dp(&val->_int);
+}
+
+extern void avalue_to_int(avalue* val, uint bits)
+{
+        if (avalue_is_float(val))
+        {
+                val->_integer = true;
+                val->_int     = float_to_int(&val->_float, bits);
+        }
+        else
+                int_resize(&val->_int, bits);
+}
+
+extern void avalue_to_sp(avalue* val)
+{
+        if (avalue_is_float(val))
+                float_to_sp(&val->_float);
+        else
+        {
+                val->_integer = false;
+                val->_float   = int_to_sp(&val->_int);
+        }
+}
+
+extern void avalue_to_dp(avalue* val)
+{
+        if (avalue_is_float(val))
+                float_to_dp(&val->_float);
+        else
+        {
+                val->_integer = false;
+                val->_float   = int_to_dp(&val->_int);
+        }
+}
---
+++ lib/libscl/value.h
@@ -0,0 +1,159 @@
+#ifndef SVALUE_H
+#define SVALUE_H
+
+#ifdef S_HAS_PRAGMA
+#pragma once
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "common.h"
+
+typedef enum
+{
+        CR_EQ,
+        CR_LE,
+        CR_GR,
+} cmp_result;
+
+typedef enum
+{
+        OR_OK,
+        OR_DIV_BY_ZERO,
+        OR_OVERFLOW,
+        OR_UNDERFLOW,
+        OR_INVALID,
+} op_result;
+
+typedef struct _int_value int_value;
+
+typedef enum
+{
+        FP_SINGLE,
+        FP_DOUBLE,
+} float_precision;
+
+typedef struct _float_value
+{
+        float_precision _precision;
+        union
+        {
+                float  _float;
+                double _double;
+        };
+} float_value;
+
+extern void       float_init_sp(float_value* self, float v);
+extern void       float_init_dp(float_value* self, double v);
+
+extern float_precision float_get_precision(const float_value* self);
+
+extern op_result  float_add(float_value* self, const float_value* rhs);
+extern op_result  float_sub(float_value* self, const float_value* rhs);
+extern op_result  float_mul(float_value* self, const float_value* rhs);
+extern op_result  float_div(float_value* self, const float_value* rhs);
+extern cmp_result float_cmp(const float_value* lhs, const float_value* rhs);
+
+extern bool       float_is_zero(const float_value* val);
+extern bool       float_is_inf(const float_value* val);
+extern bool       float_is_nan(const float_value* val);
+extern bool       float_is_sp(const float_value* val);
+extern bool       float_is_dp(const float_value* val);
+                  
+extern void       float_to_dp(float_value* val);
+extern void       float_to_sp(float_value* val);
+extern int_value  float_to_int(const float_value* val, uint bits);
+
+extern float      float_get_sp(const float_value* val);
+extern double     float_get_dp(const float_value* val);
+extern suint8     float_get8(const float_value* val);
+extern suint16    float_get16(const float_value* val);
+extern suint32    float_get32(const float_value* val);
+extern suint64    float_get64(const float_value* val);
+
+typedef struct _int_value
+{
+        uint    _bits;
+        suint64 _val;
+} int_value;
+
+extern void       int_init(int_value* self, uint bits, suint64 val);
+
+extern uint       int_get_bits(const int_value* self);
+
+extern op_result  int_add(int_value* self, const int_value* rhs);
+extern op_result  int_sub(int_value* self, const int_value* rhs);
+extern op_result  int_mul(int_value* self, const int_value* rhs);
+extern op_result  int_div(int_value* self, const int_value* rhs);
+extern op_result  int_mod(int_value* self, const int_value* rhs);
+extern op_result  int_shl(int_value* self, const int_value* rhs);
+extern op_result  int_shr(int_value* self, const int_value* rhs);
+extern op_result  int_and(int_value* self, const int_value* rhs);
+extern op_result  int_xor(int_value* self, const int_value* rhs);
+extern op_result  int_or(int_value* self, const int_value* rhs);
+
+extern cmp_result int_cmp(const int_value* lhs, const int_value* rhs);
+
+extern bool       int_is_zero(const int_value* val);
+
+extern suint8     int_get8(const int_value* val);
+extern suint16    int_get16(const int_value* val);
+extern suint32    int_get32(const int_value* val);
+extern suint64    int_get64(const int_value* val);
+extern suint64    int_get_max(const int_value* val);
+extern float      int_get_sp(const int_value* val);
+extern double     int_get_dp(const int_value* val);
+
+extern void        int_resize(int_value* val, uint bits);
+extern float_value int_to_sp(const int_value* val);
+extern float_value int_to_dp(const int_value* val);
+
+typedef struct _value
+{
+        bool _integer;
+        union
+        {
+                float_value _float;
+                int_value   _int;
+        };
+} avalue;
+
+extern void       avalue_init_int(avalue* self, uint bits, suint64 val);
+extern void       avalue_init_sp(avalue* self, float v);
+extern void       avalue_init_dp(avalue* self, double v);
+
+extern op_result  avalue_add(avalue* self, const avalue* rhs);
+extern op_result  avalue_sub(avalue* self, const avalue* rhs);
+extern op_result  avalue_mul(avalue* self, const avalue* rhs);
+extern op_result  avalue_div(avalue* self, const avalue* rhs);
+extern op_result  avalue_mod(avalue* self, const avalue* rhs);
+extern op_result  avalue_shl(avalue* self, const avalue* rhs);
+extern op_result  avalue_shr(avalue* self, const avalue* rhs);
+extern op_result  avalue_and(avalue* self, const avalue* rhs);
+extern op_result  avalue_xor(avalue* self, const avalue* rhs);
+extern op_result  avalue_or(avalue* self, const avalue* rhs);
+
+extern cmp_result avalue_cmp(const avalue* lhs, const avalue* rhs);
+
+extern bool       avalue_is_zero(const avalue* val);
+extern bool       avalue_is_float(const avalue* val);
+extern bool       avalue_is_int(const avalue* val);
+
+extern suint8     avalue_get8(const avalue* val);
+extern suint16    avalue_get16(const avalue* val);
+extern suint32    avalue_get32(const avalue* val);
+extern suint64    avalue_get64(const avalue* val);
+extern float      avalue_get_sp(const avalue* val);
+extern double     avalue_get_dp(const avalue* val);
+
+extern void       avalue_to_int(avalue* val, uint bits);
+extern void       avalue_to_sp(avalue* val);
+extern void       avalue_to_dp(avalue* val);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // !SVALUE_H
