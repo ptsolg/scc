d9b3a616f229f279014133362bb56842423af83b
ptslog <hsfk@yandex.ru>
2018-01-23 22:48:10 +1000

    ssa: update ssa.h

--- include/scc/ssa/ssa.h
+++ include/scc/ssa/ssa.h
@@ -10,14 +10,17 @@ extern "C" {
 #endif
 
 #include "ssa-block.h"
-#include "ssa-branch.h"
 #include "ssa-builder.h"
+#include "ssa-common.h"
 #include "ssa-context.h"
+#include "ssa-function.h"
 #include "ssa-instr.h"
+#include "ssaizer.h"
+#include "ssa-module.h"
+#include "ssa-opt.h"
+#include "ssa-pass.h"
 #include "ssa-printer.h"
 #include "ssa-value.h"
-#include "ssa-module.h"
-#include "ssaizer.h"
 
 #ifdef __cplusplus
 }
