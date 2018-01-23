fdd375665aed85357c2eddb9c6b85499e9714f5f
ptslog <hsfk@yandex.ru>
2018-01-23 23:12:20 +1000

    heap-debug: implement read-only page insertion before allocated memory block

--- include/scc/scl/heap-debug.h
+++ include/scc/scl/heap-debug.h
@@ -12,9 +12,14 @@ extern "C" {
 #include "common.h"
 
 // inserts read-only page of memory after each allocated block of memory
-extern void* __cdecl hd_malloc(size_t size);
-extern void __cdecl hd_free(void* block);
-extern void scl_enable_heap_debug();
+extern void* __cdecl hd_malloc_a(size_t size);
+extern void __cdecl hd_free_a(void* block);
+
+// inserts read-only page of memory before each allocated block of memory
+extern void* __cdecl hd_malloc_b(size_t size);
+extern void __cdecl hd_free_b(void* block);
+
+extern void scl_enable_heap_debug(bool insert_after);
 
 #ifdef __cplusplus
 }
--- lib/scl/heap-debug.c
+++ lib/scl/heap-debug.c
@@ -6,7 +6,7 @@
 #include <math.h>
 #define PAGE_SIZE 4096
 
-extern void* __cdecl hd_malloc(ssize size)
+extern void* __cdecl hd_malloc_a(ssize size)
 {
         ssize available = (ssize)ceil((double)size / (double)PAGE_SIZE) * PAGE_SIZE;
         ssize protected_ = PAGE_SIZE;
@@ -19,7 +19,25 @@ extern void* __cdecl hd_malloc(ssize size)
         return p + available - size;
 }
 
-extern void __cdecl hd_free(void* block)
+extern void __cdecl hd_free_a(void* block)
+{
+        VirtualFree(block, 0, MEM_RELEASE);
+}
+
+extern void* __cdecl hd_malloc_b(ssize size)
+{
+        ssize available = (ssize)ceil((double)size / (double)PAGE_SIZE) * PAGE_SIZE;
+        ssize protected_ = PAGE_SIZE;
+        DWORD old;
+
+        char* p = VirtualAlloc(NULL, available + protected_, MEM_COMMIT, PAGE_READWRITE);
+        if (!p)
+                return NULL;
+        VirtualProtect(p, protected_, PAGE_READONLY, &old);
+        return p + protected_;
+}
+
+extern void __cdecl hd_free_b(void* block)
 {
         VirtualFree(block, 0, MEM_RELEASE);
 }
@@ -28,19 +46,33 @@ extern void __cdecl hd_free(void* block)
 
 #include "scc/scl/malloc.h"
 
-extern void* __cdecl hd_malloc(size_t size)
+extern void* __cdecl hd_malloc_a(size_t size)
 {
         return smalloc(size);
 }
 
-extern void __cdecl hd_free(void* block)
+extern void __cdecl hd_free_a(void* block)
 {
         sfree(block);
 }
 
+extern void* __cdecl hd_malloc_b(size_t size)
+{
+        return smalloc(size);
+}
+
+extern void __cdecl hd_free_b(void* block)
+{
+        sfree(block);
+}
+
+
 #endif
 
-extern void scl_enable_heap_debug()
+extern void scl_enable_heap_debug(bool insert_after)
 {
-        scl_override_malloc(&hd_malloc, &hd_free);
+        if (insert_after)
+                scl_override_malloc(&hd_malloc_a, &hd_free_a);
+        else
+                scl_override_malloc(&hd_malloc_b, &hd_free_b);
 }
