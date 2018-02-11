bef5b8e5b0b6ba9d61f994718035dc71ade9e730
ptslog <hsfk@yandex.ru>
2018-02-11 19:47:24 +1000

    c-sema: move decl-related functions to c-sema-decl.*

--- include/scc/c/c-sema.h
+++ include/scc/c/c-sema.h
@@ -10,13 +10,13 @@ extern "C" {
 #endif
 
 #include "c-error.h"
-#include "c-tree.h"
+#include "c-context.h"
 
 typedef struct
 {
         tree_stmt* switch_stmt;
         htab used_values;
-        bool has_default;
+        bool has_default_label;
 } cswitch_stmt_info;
 
 // The structure, used for semantic analysis and building AST
@@ -53,56 +53,11 @@ extern void csema_push_scope(csema* self);
 
 extern void csema_push_switch_stmt_info(csema* self, tree_stmt* switch_stmt);
 extern void csema_pop_switch_stmt_info(csema* self);
-extern void csema_set_switch_stmt_has_default(csema* self);
+extern void csema_set_switch_stmt_has_default_label(csema* self);
 extern cswitch_stmt_info* csema_get_switch_stmt_info(const csema* self);
 extern bool csema_in_switch_stmt(const csema* self);
-extern bool csema_switch_stmt_has_default(const csema* self);
+extern bool csema_switch_stmt_has_default_label(const csema* self);
 extern bool csema_switch_stmt_register_case_label(const csema* self, tree_stmt* label);
-
-extern tree_decl* csema_get_any_decl(
-        const csema* self,
-        const tree_decl_scope* scope,
-        tree_id name,
-        bool parent_lookup);
-
-extern tree_decl* csema_get_decl(
-        const csema* self,
-        const tree_decl_scope* scope,
-        tree_id name,
-        bool is_tag,
-        bool parent_lookup);
-
-extern tree_decl* csema_get_local_decl(const csema* self, tree_id name, bool is_tag);
-extern tree_decl* csema_get_global_decl(const csema* self, tree_id name, bool is_tag);
-extern tree_decl* csema_get_label_decl(const csema* self, tree_id name);
-
-extern tree_decl* csema_require_decl(
-        const csema* self,
-        const tree_decl_scope* scope,
-        tree_location name_loc,
-        tree_decl_kind kind,
-        tree_id name,
-        bool parent_lookup);
-
-extern tree_decl* csema_require_local_decl(
-        const csema* self, tree_location name_loc, tree_decl_kind kind, tree_id name);
-
-extern tree_decl* csema_require_global_decl(
-        const csema* self, tree_location name_loc, tree_decl_kind kind, tree_id name);
-
-extern tree_decl* csema_require_label_decl(
-        const csema* self, tree_location name_loc, tree_id name);
-
-extern tree_decl* csema_require_member_decl(
-        const csema* self, tree_location name_loc, const tree_decl* record, tree_id name);
-
-extern bool csema_require_complete_type(
-        const csema* self, tree_location loc, const tree_type* type);
-
-extern void csema_init_dseq_ptr(csema* self, dseq* group);
-extern tree_module* csema_finish_module(csema* self);
-
-extern const char* csema_get_id_cstr(const csema* self, tree_id id);
 
 extern bool csema_at_file_scope(const csema* self);
 extern bool csema_at_block_scope(const csema* self);
