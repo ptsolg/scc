3c3f585dc0d0999aa48ae74f1f7e5cf7d5fbeae8
ptslog <hsfk@yandex.ru>
2018-06-22 10:46:39 +1000

    stdio.h: add FILE, fopen, fclose

--- libstdc/stdio.h
+++ libstdc/stdio.h
@@ -3,4 +3,13 @@
 
 extern int printf(const char*, ...);
 
+typedef struct
+{
+	void* placeholder;
+} FILE;
+
+extern FILE* fopen(const char* file, const char* mode);
+extern int fclose(FILE* file);
+extern int fprintf(FILE* file, const char* format, ...);
+
 #endif
