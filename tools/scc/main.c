74da030ffda4be3ba8c3166cc5cbbea18ebbaec2
Sergei <hsfk@yandex.ru>
2021-03-05 01:44:36 -0800

    Remove ast-wrapper

--- tools/scc/main.c
+++ tools/scc/main.c
@@ -5,8 +5,10 @@ int main(int argc, const char** argv)
         errcode result = EC_ERROR;
         scc_env env;
         scc_init(&env);
+
         if (EC_SUCCEEDED(scc_setup(&env, argc, argv)))
                 result = cc_run(&env.cc);
+        
         scc_dispose(&env);
         return result;
 }
