491374995ecab1401f1c3c98b3fdca3dbe411627
Sergei <hsfk@yandex.ru>
2021-05-13 02:41:34 -0700

    preprocessor-expression: Include num.h

--- lib/lex/preprocessor-expression.h
+++ lib/lex/preprocessor-expression.h
@@ -2,10 +2,10 @@
 #define C_PREPROCESSOR_EXPRESSION_H
 
 #include "scc/core/common.h"
+#include "scc/core/num.h"
 
 typedef struct _c_preprocessor c_preprocessor;
 typedef struct _c_token c_token;
-struct num;
 
 // returns token after expression
 extern c_token* c_preprocessor_evaluate_expr(c_preprocessor* self, struct num* result);
