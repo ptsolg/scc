ca924de1fe3a0dac60397254af971575541f6a16
Sergei <hsfk@yandex.ru>
2021-05-16 01:17:38 -0700

    vec: Add foreach macro

--- include/scc/core/vec.h
+++ include/scc/core/vec.h
@@ -5,4 +5,8 @@
 #define VEC_T void*
 #include "vec.inc"
 
+#define VEC_FOREACH(PVEC, IT, END)\
+        for (void** IT = vec_begin(PVEC), **END = vec_end(PVEC);\
+                IT != END; (IT)++)
+
 #endif
