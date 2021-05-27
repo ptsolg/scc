846b03176c5dab47fa0797306d0f12d70ad998ce
Sergei <hsfk@yandex.ru>
2021-05-27 00:18:07 -0700

    libstdc: Add strstr

--- libstdc/include/string.h
+++ libstdc/include/string.h
@@ -17,6 +17,7 @@ extern char* strchr(const char* s, int c);
 extern char* strcpy(char* dst, const char* src);
 extern char* strncpy(char* dst, const char* src, size_t num);
 extern char* strcat(char* dst, const char* src);
+extern char* strstr(const char* s1, const char* s2);
 
 typedef char* va_list;
 extern int vsnprintf(char* s, size_t n, const char* format, va_list arg);
